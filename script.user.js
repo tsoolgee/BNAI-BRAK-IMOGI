a043dc4b78df8b33d843c9ebe0a34733ed4e35db
tsoolgee <tsoolgee@gmail.com>
2026-06-16 11:50:01 +0300

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         בני ברק - אימוג'י
 // @namespace    https://github.com/tsoolgee/BNAI-BRAK-IMOGI
-// @version      0.0.2
+// @version      0.0.5
 // @description  המרת טקסט לאימוג'ים באתר bnebrak.com
 // @author       You
 // @match        https://bnebrak.com/*
@@ -31,20 +31,43 @@
         return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
     }
 
+    function isWordChar(ch) {
+        if (!ch) return false;
+        // אות, ספרה, גרש, מקף = תו מילה. רווח, פיסוק, נקודותיים = לא תו מילה
+        return /[\u0590-\u05FFa-zA-Z0-9'`״׳\-]/.test(ch);
+    }
+
     function processText(node) {
         if (node.nodeType !== Node.TEXT_NODE) return;
         let text = node.nodeValue;
         const original = text;
+
         for (const [from, to] of map) {
-            // המילה חייבת להיות מוקפת ברווח, סימן פיסוק, או תחילת/סוף שורה
-            const regex = new RegExp(
-                '(^|[\\s.,!?;:\'"()\\[\\]])'
-                + escapeRegex(from)
-                + '(?=[\\s.,!?;:\'"()\\[\\]]|$)',
-                'g'
-            );
-            text = text.replace(regex, (match, prefix) => prefix + to);
+            const escaped = escapeRegex(from);
+            const regex = new RegExp(escaped, 'g');
+            let match;
+            let result = '';
+            let lastIndex = 0;
+
+            while ((match = regex.exec(text)) !== null) {
+                const start = match.index;
+                const end = start + from.length;
+
+                const charBefore = text[start - 1];
+                const charAfter = text[end];
+
+                if (!isWordChar(charBefore) && !isWordChar(charAfter)) {
+                    result += text.slice(lastIndex, start) + to;
+                } else {
+                    result += text.slice(lastIndex, end);
+                }
+                lastIndex = end;
+            }
+
+            result += text.slice(lastIndex);
+            text = result;
         }
+
         if (text !== original) node.nodeValue = text;
     }
 
